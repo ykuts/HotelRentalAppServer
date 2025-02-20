82c5e9bfa3fb1ee391198fce09b5abc74c24dbc5
nabya47 <nabyanaseem47@gmail.com>
2025-02-20 19:27:23 +0500

    Update server.js

--- server.js
+++ server.js
@@ -18,20 +18,20 @@ connectDB();
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
-  origin: ["https://tourmaline-pixie-dbc701.netlify.app", "https://spiffy-torte-57b54c.netlify.app/" ],
-  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-  allowedHeaders: [
-      'Content-Type',
-      'Authorization',
-      'Access-Control-Allow-Headers',
-      'Access-Control-Allow-Methods',
-      'method',
-      'X-Requested-With'
-  ],
-  exposedHeaders: ['Content-Range', 'X-Content-Range'],
-  credentials: true,
-  preflightContinue: false,
-  optionsSuccessStatus: 204
+  origin: ["https://tourmaline-pixie-dbc701.netlify.app", "https://spiffy-torte-57b54c.netlify.app" ],
+//   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+//   allowedHeaders: [
+//       'Content-Type',
+//       'Authorization',
+//       'Access-Control-Allow-Headers',
+//       'Access-Control-Allow-Methods',
+//       'method',
+//       'X-Requested-With'
+//   ],
+//   exposedHeaders: ['Content-Range', 'X-Content-Range'],
+//   credentials: true,
+//   preflightContinue: false,
+//   optionsSuccessStatus: 204
 }));
 
 // setup routes
