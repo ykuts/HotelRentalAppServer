944dcad58b9e14b034eb6a15c11eedd54bd62df5
nabya47 <nabyanaseem47@gmail.com>
2025-03-27 23:18:24 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const dotenv = require("dotenv").config();
-// const mongoose = require("mongoose");
 const express = require("express");
 const cors = require('cors');
 const { errorHandler } = require("./middleware/errorHandler");
