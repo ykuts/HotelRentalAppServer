183f373dc968462687f7722c3d7359ca4b9c89c8
nabya47 <nabyanaseem47@gmail.com>
2025-04-11 16:34:00 +0500

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const connectDB = require("./config/db");
 const roomRoutes = require("./routes/roomRoutes");
 const bookingRoutes = require("./routes/bookingRoutes");
 const userRoutes = require("./routes/userRoutes");
-const cookieParser = require("cookie-parser");
+// const cookieParser = require("cookie-parser");
 
 const port = process.env.PORT || 5000;
 // const port = dotenv.env.PORT || 5000;
@@ -17,55 +17,32 @@ const port = process.env.PORT || 5000;
 connectDB();
 
 // setup middlewares
-app.use(cookieParser());
-// app.use(cors({
-//   origin: [
-//     "https://tourmaline-pixie-dbc701.netlify.app",
-//     "https://spiffy-torte-57b54c.netlify.app"
-//   ],
-//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//   credentials: true,
-// }));
+// app.use(cookieParser());
+
 
 // app.use(cors({
-//   origin: [
-//     "https://tourmaline-pixie-dbc701.netlify.app",
-//     "https://spiffy-torte-57b54c.netlify.app"
+//   origin:'*',
+//   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+//   allowedHeaders: [
+//       'Content-Type',
+//       'Authorization',
+//       'Access-Control-Allow-Headers',
+//       'Access-Control-Allow-Methods',
+//       'method',
+//       'X-Requested-With'
 //   ],
-//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//   // allowedHeaders: [
-//   //   'Content-Type',
-//   //         'Authorization',
-//   //         'Access-Control-Allow-Headers',
-//   //         'Access-Control-Allow-Methods',
-//   //         'method',
-//   //         'X-Requested-With'
-//   // ],
-//   // exposedHeaders: ['Content-Range', 'X-Content-Range'],
+//   exposedHeaders: ['Content-Range', 'X-Content-Range'],
 //   credentials: true,
-//   // preflightContinue: false,
-//   // optionsSuccessStatus: 204
+//   preflightContinue: false,
+//   optionsSuccessStatus: 204
 // }));
-
 
 app.use(cors({
-  origin:'*',
+  origin: '*', // or replace with your frontend URL
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-  allowedHeaders: [
-      'Content-Type',
-      'Authorization',
-      'Access-Control-Allow-Headers',
-      'Access-Control-Allow-Methods',
-      'method',
-      'X-Requested-With'
-  ],
+  allowedHeaders: ['Content-Type', 'Authorization'],
   exposedHeaders: ['Content-Range', 'X-Content-Range'],
-  credentials: false,
-  preflightContinue: false,
-  optionsSuccessStatus: 204
 }));
-
-app.options('*', cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
