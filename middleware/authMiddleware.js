d528c06d3fff286797de562cae50ed07c0d93ebc
yuliiak <98550411+ykuts@users.noreply.github.com>
2025-05-09 14:18:42 +0200

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -3,21 +3,28 @@ const User = require("../models/userModel");
 
 const auth = async (req, res, next) => {
   try {
+    console.log("Checking authorization");
+    console.log("Headers:", req.headers);
+    
     const token = req.cookies.jwt;
     
     const authHeader = req.headers.authorization;
     if (authHeader && authHeader.startsWith('Bearer ')) {
       token = authHeader.split(' ')[1];
+      console.log("Token from header:", token ? "Received" : "Empty");
     }
     
     if (!token) {
+      console.log("Token is empty");
       return res.status(400).json({ message: "no token" });
     }
 
     //verify the token
     const data = jwt.verify(token, process.env.JWT_SECRET);
+    console.log("Token cheked, User ID:", data.id);
     const user= await User.findById(data.id);
     if (!user) {
+      console.log("User not found");
       return res.status(400).json({ message: "invalid user" });
     }
 
